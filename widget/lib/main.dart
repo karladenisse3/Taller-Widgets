89e4583703189f2f024746d50802d9454e3f3394
karladenisse3 <karlafernndez00@hotmail.com>
2022-07-14 09:42:51 -0500

    Taller

--- widget/lib/main.dart
+++ widget/lib/main.dart
@@ -60,56 +60,43 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
+ bool animated = false;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Taller Widget"),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            AnimatedDefaultTextStyle(
+              child: Text('APLICACIONES MÓVILES II -- A'),
+              style : animated ? TextStyle(
+                color: Color.fromARGB(255, 255, 0, 111),
+                fontSize: 26,
+              ) : TextStyle(
+                color: Color.fromARGB(255, 240, 95, 187),
+                fontSize: 14,
+              ),
+              duration: Duration(milliseconds: 200),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            SizedBox(
+              height: 50,
             ),
+            RaisedButton(
+              child: Text('AnimatedDefaultTextStyle'),
+              onPressed: () {
+                setState(() {
+                  animated = !animated;
+                });
+              },
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
